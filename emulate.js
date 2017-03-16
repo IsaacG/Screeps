e8729f2770cdff471c78990de95e534d5acb28fd
Isaac Good <github@isaacgood.com>
2017-03-16 01:10:22 -0700

    Tidy up

--- emulate.js
+++ emulate.js
@@ -1,12 +1,32 @@
 
-Game = {time:0, spawns:{1:{id:1,canCreateCreep:function(){return -4}, spawning:false}}, creeps:{}, getObjectById: function(){}};
+Game = {
+  time:0,
+  spawns: {
+    'Spawn1': {
+      id:12345,
+      canCreateCreep:function(){return -4},
+      spawning:false,
+      name: 'Spawn1',
+    }
+  },
+  rooms : {
+    W5N8: {
+      name: 'W5N8'
+    }
+  },
+  creeps:{},
+  getObjectById: function(){}
+};
+
 Memory = {}
 Memory.log = {level: 1, klass: 'Scheduler', pid: 0};
 var s = require('main');
 
 function run() {
-  console.log('Game Time = ' + Game.time++);
+  console.log('==== ' + Game.time++ + ' ====');
   s.loop();
   if (Game.time < 20) setTimeout(run, 2000);
 }
 run();
+
+// vim:syntax=javascript:expandtab:ts=2:sw=2
