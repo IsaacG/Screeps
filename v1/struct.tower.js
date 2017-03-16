7f8354632d164f283fc4ecc7ab467dd14261722e
Isaac Good <github@isaacgood.com>
2017-03-15 22:22:59 -0700

    Make the tower repair selection slightly smarter

--- v1/struct.tower.js
+++ v1/struct.tower.js
@@ -1,17 +1,67 @@
-var structTower = {
-    run: function(tower) {
-        var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-            filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if(closestDamagedStructure) {
-            tower.repair(closestDamagedStructure);
-        }
+function repairRanker(tower, structure) {
+  if (!tower) { console.log("No tower passed."); return null; }
+  if (!structure) { console.log("No structure passed."); return null; }
+  var value = structure.hits / structure.hitsMax;
+  value *= (structure.structureType == STRUCTURE_RAMPART ? 7 : 1);
+  value /= Math.sqrt(tower.pos.getRangeTo(structure));
+  return value;
+}
 
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if(closestHostile) {
-            tower.attack(closestHostile);
-        }
+function findClosestDamaged(tower) {
+    var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+      filter: (structure) => structure.hits < structure.hitsMax
+    });
+    return closestDamagedStructure;
+}
+
+function findBestToRepair(tower) {
+	if (!(Memory.tower_repair_target)) Memory.tower_repair_target = {};
+
+	var repair = Memory.tower_repair_target;
+
+	if (!(repair[tower.id]) || (Game.time % 15) === 0) {
+	  repair[tower.id] = null;
+    var targets = tower.room.find(FIND_STRUCTURES);
+    targets = targets.filter((s) => {return s.hits < s.hitsMax});
+    targets = targets.filter((s) => {
+      return (s.structureType == STRUCTURE_RAMPART ||
+        s.structureType == STRUCTURE_ROAD ||
+        s.structureType == STRUCTURE_WALL);
+    });
+    if (targets.length > 0) {
+      var willDie = targets.filter((s) => {
+        return (s.structureType == STRUCTURE_RAMPART && s.hits < 2*RAMPART_DECAY_AMOUNT) ||
+           (s.structureType == STRUCTURE_ROAD && s.hits < 2*ROAD_DECAY_AMOUNT); });
+      if (willDie.length > 0) targets = willDie;
+      if (targets.length > 1) {
+        targets.sort((a,b) => {
+          return repairRanker(tower, a) - repairRanker(tower, b); });
+      }
+      repair[tower.id] = targets[0].id;
     }
+	}
+  return Game.getObjectById(repair[tower.id]);
+}
+
+var structTower = {
+  run: function(tower) {
+	
+    if (false) {
+      var repair = findClosestDamaged(tower);
+    } else {
+      var repair = findBestToRepair(tower);
+    }
+    if(repair) {
+      tower.repair(repair);
+    }
+
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if(closestHostile) {
+      tower.attack(closestHostile);
+    }
+  }
 };
 
 module.exports = structTower;
+
+// vim:syntax=javascript:expandtab:ts=2:sw=2
