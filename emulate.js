5b9a3aece6bd3f3c941826e4e539c054c6dd455a
Isaac Good <github@isaacgood.com>
2017-03-15 22:20:33 -0700

    Slow down the emulator and point it at main.js

--- emulate.js
+++ emulate.js
@@ -1,11 +1,11 @@
 
 Game = {time:0, spawns:{1:{id:1,canCreateCreep:function(){return -4}, spawning:false}}, creeps:{}, getObjectById: function(){}};
 Memory = {}
-var s = require('scheduler');
+var s = require('main');
 
 function run() {
   console.log('Game Time = ' + Game.time++);
   s.loop();
-  if (Game.time < 20) setTimeout(run, 1000);
+  if (Game.time < 20) setTimeout(run, 2000);
 }
 run();
