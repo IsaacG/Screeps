00d9e218744057ef4d90cfd3c57ce29f77e85033
Isaac Good <github@isaacgood.com>
2017-03-21 01:14:14 -0700

    Tidy code up

--- v1/role.harvester.js
+++ v1/role.harvester.js
@@ -1,36 +1,63 @@
-var roleHarvester = {
-    run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-	    if (!creep.memory.source) {
-                var sources = creep.room.find(FIND_SOURCES);
-		var i = Math.floor(Math.random() * sources.length);
-		creep.memory.source = sources[i].id;
-	    }
-	    var source = Game.getObjectById(creep.memory.source);
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {visualizePathStyle: {stroke: '#aaaaff'}});
-            }
-            return true;
-        }
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (((structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity) ||
-                        (structure.structureType == STRUCTURE_STORAGE && structure.store < structure.storeCapacity));
-                }
-            });
-            if(targets.length > 0) {
-                targets.sort((a,b)=>{return (a.energy/a.energyCapacity)-(b.energy/b.energyCapacity);});
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#aaffff'}});
-                }
-                return true;
-            }
-        }
-        return false;
+function getSource (creep) {
+  var sources = creep.room.find(FIND_SOURCES);
+  var i = Math.floor(Math.random() * sources.length);
+  return sources[i].id;
+}
+
+function getTarget (creep) {
+  var targets = creep.room.find(FIND_STRUCTURES, {
+    filter: (structure) => {
+      return (((structure.structureType == STRUCTURE_EXTENSION ||
+        structure.structureType == STRUCTURE_SPAWN ||
+        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity) ||
+        (structure.structureType == STRUCTURE_STORAGE && structure.store < structure.storeCapacity));
     }
+  });
+  if(targets.length > 0) {
+    targets.sort((a,b)=>{return (a.energy/a.energyCapacity)-(b.energy/b.energyCapacity);});
+    return targets[0].id;
+  }
+  return null;
+}
+
+var roleHarvester = {
+  run: function(creep) {
+    if (!creep.memory.task) creep.memory.task = 'harvest';
+
+    // State change
+    if (creep.memory.task === 'harvest' && creep.carry.energy === creep.carryCapacity) {
+      creep.memory.task = 'dump';
+      creep.memory.target = null;
+    } else if (creep.memory.task === 'dump' && creep.carry.energy === 0) {
+      creep.memory.task = 'harvest';
+      creep.memory.source = null;
+    }
+
+    if (creep.memory.task === 'harvest') {
+      if (!creep.memory.source) creep.memory.source = getSource(creep);
+      var source = Game.getObjectById(creep.memory.source);
+      if(creep.harvest(source) == ERR_NOT_IN_RANGE) creep.moveTo(source);
+      return true;
+    } else { // dump
+      if (!creep.memory.target) creep.memory.target = getTarget(creep);
+      if (!creep.memory.target) return false;
+      let target = Game.getObjectById(creep.memory.target);
+
+      switch(creep.transfer(target, RESOURCE_ENERGY)) {
+        case OK:
+          break;
+        case ERR_NOT_IN_RANGE:
+          creep.moveTo(target);
+          break;
+        case ERR_FULL:
+        case ERR_NOT_ENOUGH_RESOURCES:
+          creep.memory.target = null;
+          break;
+      }
+      return true;
+    }
+  }
 };
 
 module.exports = roleHarvester;
+// vim:syntax=javascript:expandtab:ts=2:sw=2
--- v1/role.repairer.js
+++ v1/role.repairer.js
@@ -1,59 +1,88 @@
-var roleRepairer = {
-    run: function(creep) {
-        if(!('source' in creep.memory) || (creep.memory.repairing && creep.carry.energy == 0)) {
-            creep.memory.repairing = false;
-            creep.say('🔄 harvest');
-            var spawn = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
-            if (spawn.energy > 200) {
-                creep.memory.source = spawn.id
-            } else {
-                creep.memory.source = creep.pos.findClosestByPath(FIND_SOURCES).id;
-            }
-        }
-        if(!creep.memory.repairing && creep.carry.energy == creep.carryCapacity) {
-            creep.memory.repairing = true;
-            creep.say('🚧 repair');
-        }
+function getSource (creep) {
+  var spawn = creep.pos.findClosestByPath(FIND_MY_SPAWNS);
+  if (spawn.energy > 200) {
+    return spawn.id;
+  } else {
+    return creep.pos.findClosestByPath(FIND_SOURCES).id;
+  }
+}
 
-        if(!creep.memory.repairing) {
-            var source = Game.getObjectById(creep.memory.source);
-            if (source.structureType == STRUCTURE_SPAWN) {
-                if(creep.withdraw(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(spawn, {visualizePathStyle: {stroke: '#ffaa00'}});
-                }
-            } else {
-                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(source, {visualizePathStyle: {stroke: '#aaaaff'}});
-                }
-            }
-            return true;
-        }
-        else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_RAMPART ||
-                        structure.structureType == STRUCTURE_ROAD ||
-                        structure.structureType == STRUCTURE_WALL) && structure.hits < structure.hitsMax;
-                }
-            });
-            if(targets.length > 0) {
-                var willDie = targets.filter((s) => {
-                    return (s.structureType == STRUCTURE_RAMPART && s.hits < 2*RAMPART_DECAY_AMOUNT) ||
-                           (s.structureType == STRUCTURE_ROAD && s.hits < 2*ROAD_DECAY_AMOUNT); });
-                if (willDie.length > 0) targets = willDie;
-                targets.sort(function(a,b){
-                    return ((a.hits/a.hitsMax)*(a.structureType == STRUCTURE_RAMPART ? 7 : 1) - 
-                            (b.hits/b.hitsMax)*(b.structureType == STRUCTURE_RAMPART ? 7 : 1))});
-                var target = targets[0];
-                // if ((Game.time % 5) == 0) console.log(`Repair target for ${creep.name} is ${target.structureType}`);
-                if(creep.repair(target) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-                return true;
-            }
-        }
-        return false;
+function getTarget (creep) {
+  var targets = creep.room.find(FIND_STRUCTURES, {
+    filter: (structure) => {
+      return (structure.structureType == STRUCTURE_RAMPART ||
+        structure.structureType == STRUCTURE_ROAD ||
+        structure.structureType == STRUCTURE_WALL) && structure.hits < structure.hitsMax;
     }
+  });
+
+  if(targets.length === 0) return null;
+
+  var willDie = targets.filter((s) => {
+    return (s.structureType == STRUCTURE_RAMPART && s.hits < 2*RAMPART_DECAY_AMOUNT) ||
+    (s.structureType == STRUCTURE_ROAD && s.hits < 2*ROAD_DECAY_AMOUNT); });
+  if (willDie.length > 0) targets = willDie;
+
+  targets.sort((a,b) => {
+    return ((a.hits/a.hitsMax)*(a.structureType == STRUCTURE_RAMPART ? 7 : 1) - 
+    (b.hits/b.hitsMax)*(b.structureType == STRUCTURE_RAMPART ? 7 : 1))});
+
+  return targets[0].id;
+}
+
+var roleRepairer = {
+  run: function(creep) {
+    if (!creep.memory.task) creep.memory.task = 'harvest';
+
+    // State change
+    if (creep.memory.task === 'harvest' && creep.carry.energy === creep.carryCapacity) {
+      creep.memory.task = 'repair';
+      creep.memory.target = null;
+    } else if (creep.memory.task === 'repair' && creep.carry.energy === 0) {
+      creep.memory.task = 'harvest';
+      creep.memory.source = null;
+    }
+
+
+    if (creep.memory.task === 'harvest') {
+      if (!creep.memory.source) creep.memory.source = getSource(creep);
+
+      var source = Game.getObjectById(creep.memory.source);
+      if (source.structureType == STRUCTURE_SPAWN) {
+        if(creep.withdraw(source, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(source);
+        }
+      } else {
+        if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(source);
+        }
+      }
+      return true;
+    } else { // repair
+      if (!creep.memory.target) creep.memory.target = getTarget(creep);
+      if (!creep.memory.target) return false;
+      let target = Game.getObjectById(creep.memory.target);
+
+      if (target.hits === target.hitsMax) {
+        creep.memory.target = null;
+        return true;
+      }
+
+      switch(creep.repair(target)) {
+        case OK:
+          break;
+        case ERR_NOT_IN_RANGE:
+          creep.moveTo(target);
+          break;
+        default:
+          creep.memory.target = null;
+          break;
+      }
+      return true;
+    }
+  }
 };
 
 module.exports = roleRepairer;
+
+// vim:syntax=javascript:expandtab:ts=2:sw=2
