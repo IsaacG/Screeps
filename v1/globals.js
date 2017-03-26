c7240d3521518602c181475eb9f2deeb19b9b213
Isaac Good <github@isaacgood.com>
2017-03-25 22:44:20 -0700

    Add a suicide(). Fix list to match new memory layout.

--- v1/globals.js
+++ v1/globals.js
@@ -1,2 +1,3 @@
 global.build = (x) => Game.spawns['Spawn1'].memory.makeCreep.push(x)
-global.list = () => Object.keys(Game.creeps).map((k,i)=>{return k + ':' + Game.creeps[k].memory.role}) 
+global.list = () => Object.keys(Game.creeps).map((k,i)=>{return k + ':' + JSON.stringify(Game.creeps[k].memory.init)}) 
+global.suicide = (x) => Game.creeps[x].suicide()
