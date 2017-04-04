847e584345a790088781960a31bd5e547381242b
Isaac Good <github@isaacgood.com>
2017-04-04 01:14:36 -0700

    build() support for multi-spawn

--- v1/globals.js
+++ v1/globals.js
@@ -1,4 +1,4 @@
-global.build = (x) => Game.spawns['Spawn1'].memory.makeCreep.push(x)
+global.build = (x,y) => {if (!y) y='Spawn1'; Game.spawns[y].memory.makeCreep.push(x);}
 global.list = () => Object.keys(Game.creeps).map((k,i)=>{return k + ':' + JSON.stringify(Game.creeps[k].memory.init)}) 
 global.li = (x) => {let r={}; Game.rooms[x].find(FIND_MY_CREEPS).forEach((c)=>{let l = c.memory.init.role; if (!(l in r)) r[l]=[]; r[l].push(`${c.name}(${c.body.length})`);}); return JSON.stringify(r);}
 global.l = () => {let o =""; for(var n in Game.rooms){o += `${n}: ${li(n)}\n`;}; return o;}
