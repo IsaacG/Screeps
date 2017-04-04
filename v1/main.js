391a541104861103efa3f21fa5d4641c674415ae
Isaac Good <github@isaacgood.com>
2017-04-04 01:15:01 -0700

    More support for multi-spawn

--- v1/main.js
+++ v1/main.js
@@ -18,35 +18,54 @@ module.exports.loop = function () {
     if (!('makeCreep' in spawn.memory)) spawn.memory.makeCreep = [{role: 'harvester'}, {role: 'upgrader'}, {role: 'builder'}];
 
   // Auto spawn slowly up to the desired count.
-  if ((Game.time % 50) === 0) {
-    var role_count = {harvester: 0, upgrader: 0, builder: 0, repairer: 0};
+  if ((Game.time % 25) === 0) {
     var desired_count = {
-            harvester: 1,
            harvester_s: 2,
            hauler: 1,
            upgrader: 3,
            builder: 1,
            repairer: 1,
     }
-    Object.keys(Game.creeps).forEach((c) => {
-      if (role_count[Game.creeps[c].memory.init.role]) {
-        role_count[Game.creeps[c].memory.init.role]++;
+    for (var name in Game.rooms) {
+      let source_count  = Game.rooms[name].find(FIND_SOURCES).length;
+      desired_count.harvester_s = source_count;
+      desired_count.upgrader = source_count + 1;
+      desired_count.repairer = source_count - 1;
+      let construction = Game.rooms[name].find(FIND_CONSTRUCTION_SITES).length;
+      if (construction == 0) {
+        desired_count.builder = 0;
+      } else if (construction < 10) {
+        desired_count.builder = 1;
       } else {
-        role_count[Game.creeps[c].memory.init.role] = 1;
+        desired_count.builder = 2;
       }
+
+      var role_count = {harvester: 0, upgrader: 0, builder: 0, repairer: 0, hauler: 0, harvester_s: 0};
+      Game.rooms[name].find(FIND_MY_CREEPS).forEach((c) => {
+        role = c.memory.init.role;
+        if (role_count[role]) {
+          role_count[role]++;
+        } else {
+          role_count[role] = 1;
+        }
       });
-    Object.keys(desired_count).forEach((r) => {
-      if (spawn.memory.makeCreep.length > 0) return;
-      if ((!role_count[r]) || desired_count[r] > role_count[r]) {
-        console.log(`For role ${r} we desire ${desired_count[r]} and have ${role_count[r]}. Spawn more.`);
-        spawn.memory.makeCreep.push(r);
-      }
-    });
+      let s = Game.rooms[name].find(FIND_MY_SPAWNS)[0];
+      if (role_count.harvester_s == 0 || role_count.hauler == 0) s.memory.makeCreep.push('harvester');
+      Object.keys(desired_count).forEach((r) => {
+        if (s.memory.makeCreep.length > 0) return;
+        if ((!role_count[r]) || desired_count[r] > role_count[r]) {
+          console.log(`For role ${r} we desire ${desired_count[r]} and have ${role_count[r]}. Spawn more.`);
+          s.memory.makeCreep.push(r);
+        }
+      });
+    }
   }
 
-  var towers = spawn.room.find(FIND_STRUCTURES, {
-    filter: (structure) => {return (structure.structureType == STRUCTURE_TOWER)}});
-  towers.forEach(tower => structTower.run(tower));
+  for(var name in Game.rooms) {
+    var towers = Game.rooms[name].find(FIND_STRUCTURES, {
+      filter: (structure) => {return (structure.structureType == STRUCTURE_TOWER)}});
+    towers.forEach(tower => structTower.run(tower));
+  }
 
   if ((Game.time % 10) == 0) {
     for (var name in Game.spawns) {
@@ -62,7 +81,7 @@ module.exports.loop = function () {
     if (!('role' in creep.memory.init)) creep.memory.init.role = 'harvester';
     var role = creep.memory.init.role;
 
-    if (creep.ticksToLive == 50 && !creep.memory.pushedRegen) {
+    if (creep.ticksToLive == 5 && !creep.memory.pushedRegen) {
       let s = creep.room.find(FIND_MY_SPAWNS)[0];
       if (!s) s = Game.spawns['Spawn1'];
       s.memory.makeCreep.push(creep.memory.init);
